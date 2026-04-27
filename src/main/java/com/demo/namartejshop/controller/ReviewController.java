be17911779efe8f7993b60d544ab72fac64cebbd
JoseMedelG <josemedel2748@gmail.com>
2026-04-27 13:38:30 +0200

    ReviewController + review-list.html

---
+++ src/main/java/com/demo/namartejshop/controller/ReviewController.java
@@ -0,0 +1,22 @@
+package com.demo.namartejshop.controller;
+
+import com.demo.namartejshop.repository.ReviewRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class ReviewController {
+
+    // Inyectar el repositorio de reviews
+    private final ReviewRepository reviewRepository;
+
+    //getmapping reviews
+    @GetMapping("reviews")
+    public String reviews(Model model) {
+        model.addAttribute("reviews", reviewRepository.findAll());
+        return "reviews/review-list";
+    }
+}
---
+++ src/main/java/com/demo/namartejshop/repository/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.demo.namartejshop.repository;
+
+import com.demo.namartejshop.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByTiendas_IdOrderByCreationDateDesc(Long tiendasId);
+
+    List<Review> findByTiendas_IdAndRatingGreaterThanEqualOrderByCreationDateDesc(Long id, Integer rating);
+
+}
