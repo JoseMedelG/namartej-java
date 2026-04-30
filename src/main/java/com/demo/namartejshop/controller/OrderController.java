446d0fd9f584119df32cf967a5ab686635fdd310
JoseMedelG <josemedel2748@gmail.com>
2026-04-30 11:15:50 +0200

    OrderController creado

---
+++ src/main/java/com/demo/namartejshop/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.demo.namartejshop.controller;
+
+
+import com.demo.namartejshop.repository.OrderLineRepository;
+import com.demo.namartejshop.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@AllArgsConstructor
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+    private final OrderLineRepository orderLineRepository;
+
+    @GetMapping("orders")
+    public String orders(Model model){
+        model.addAttribute("orders", orderRepository.findAll());
+        return "orders/order-list";
+    }
+
+    //GetMapping order/
+    @GetMapping("orders/{id}")
+    public String order(Model model, @PathVariable Long id){
+        model.addAttribute("order", orderRepository.findById(id).orElseThrow());
+        model.addAttribute("orderLines", orderLineRepository.findById(id).orElseThrow());
+        return "orders/order-detail";
+    }
+
+
+}
