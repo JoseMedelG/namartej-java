16a1a11d2be326ee621f1603412c5a01de769c1f
JoseMedelG <josemedel2748@gmail.com>
2026-05-04 13:47:13 +0200

    Filtro findByActiveTrue y findByIdAndActiveTrue agregados en TiendasRepository

--- src/main/java/com/demo/namartejshop/repository/TiendasRepository.java
+++ src/main/java/com/demo/namartejshop/repository/TiendasRepository.java
@@ -3,7 +3,16 @@ package com.demo.namartejshop.repository;
 import com.demo.namartejshop.model.Tiendas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TiendasRepository extends JpaRepository<Tiendas, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface TiendasRepository extends JpaRepository<Tiendas, Long> {
+    //    @Transactional
+    //    @Modifying
+    //    @Query("delete from Restaurant r where r.name = ?1")
+    //    void deleteByNameTodoGuay(String name);
+
+    List<Tiendas> findByActiveTrue();
+    Optional<Tiendas> findByIdAndActiveTrue(Long id);
 
 }
