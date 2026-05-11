9c1a491f6a4c3000145a4f81ae711b199ada2c1b
JoseMedelG <josemedel2748@gmail.com>
2026-05-11 10:03:46 +0200

    #5 review-form link desde tienda-detail.html

--- src/main/java/com/demo/namartejshop/controller/ReviewController.java
+++ src/main/java/com/demo/namartejshop/controller/ReviewController.java
@@ -1,19 +1,25 @@
 package com.demo.namartejshop.controller;
 
+import com.demo.namartejshop.model.Review;
+import com.demo.namartejshop.repository.ProductosRepository;
 import com.demo.namartejshop.repository.ReviewRepository;
+import com.demo.namartejshop.repository.TiendasRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @AllArgsConstructor
 public class ReviewController {
 
-    // Inyectar el repositorio de reviews
+    // Inyectar el repositorios necesarios
     private final ReviewRepository reviewRepository;
+    private final TiendasRepository tiendasRepository;
+    private final ProductosRepository productosRepository;
 
     //getmapping reviews
     @GetMapping("reviews")
@@ -33,6 +39,23 @@ public class ReviewController {
         reviewRepository.deleteById(id);
         redirectAttributes.addFlashAttribute("message", "¡Borrado exitoso!");
         return "redirect:/reviews";
+    }
+
+    @GetMapping("reviews/new")
+    public String newReview(
+            Model model,
+            @RequestParam(required = false) Long tiendaId,
+            @RequestParam(required = false) Long productoId){
+        Review review = new Review();
+
+        if(tiendaId != null)
+            review.setTiendas(tiendasRepository.findById(tiendaId).orElseThrow());
+
+        if(productoId != null)
+            review.setProductos(productosRepository.findById(productoId).orElseThrow());
+
+        model.addAttribute("review", review);
+        return "reviews/review-form";
     }
 
 
