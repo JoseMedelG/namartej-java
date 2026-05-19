7605a0523d2155c6f028551ee5cba3d82ef22729
JoseMedelG <josemedel2748@gmail.com>
2026-05-19 13:26:41 +0200

    #7 User service register method

--- src/main/java/com/demo/namartejshop/service/UserService.java
+++ src/main/java/com/demo/namartejshop/service/UserService.java
@@ -3,11 +3,13 @@ package com.demo.namartejshop.service;
 
 import com.demo.namartejshop.dto.RegisterForm;
 import com.demo.namartejshop.model.User;
+import com.demo.namartejshop.model.enums.Role;
 import com.demo.namartejshop.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,6 +19,7 @@ import java.util.Optional;
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     //metodo parta buscar el usuario en base de dtos por su username
@@ -39,7 +42,23 @@ public class UserService implements UserDetailsService {
         // cargar el usuario de base de datos por username
     }
 
-    public void register(RegisterForm form) {
-        // TODO
+    public User register(RegisterForm form) {
+
+        if(userRepository.existsByUsername(form.getUsername()))
+            throw new RuntimeException("Nombre de usuario ya existente");
+
+        if(userRepository.existsByEmail(form.getEmail()))
+            throw new RuntimeException("El email ya existe");
+
+        if(!form.getPassword().equals(form.getPasswordConfirm()))
+            throw new RuntimeException("Las contraseñas no coinciden");
+
+
+        User user = new User();
+        user.setUsername(form.getUsername());
+        user.setEmail(form.getEmail());
+        user.setPassword(passwordEncoder.encode(form.getPassword())); // password cifrada con bcrypt
+        user.setRole(Role.ROLE_USER); // por defecto todos los usuarios registrados son ROLE_USER
+        return userRepository.save(user);
     }
 }
