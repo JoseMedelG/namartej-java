3d5aec00406f7620cd3f5b647c307b6ace90438f
JoseMedelG <josemedel2748@gmail.com>
2026-05-28 09:49:46 +0200

    logger

--- src/main/java/com/demo/namartejshop/controller/UserController.java
+++ src/main/java/com/demo/namartejshop/controller/UserController.java
@@ -4,6 +4,9 @@ import com.demo.namartejshop.model.User;
 import com.demo.namartejshop.model.enums.Role;
 import com.demo.namartejshop.service.UserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Log4j
 @AllArgsConstructor
 @Controller
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private UserService userService;
 
     // user List
@@ -53,8 +58,8 @@ public class UserController {
     }
 
     @PostMapping("admin/users")
-    public String save(@ModelAttribute User user, RedirectAttributes
-            rediA) {
+    public String save(@ModelAttribute User user, RedirectAttributes rediA) {
+        log.info("Guardando user: {}", user);
 
         // Creación
         try {
@@ -68,6 +73,7 @@ public class UserController {
             }
 
         } catch (Exception e) {
+            log.warn("error al guardar user {}", user, e);
             rediA.addFlashAttribute("error", e.getMessage());
             return user.getId() == null ?
                     "redirect:/admin/users/new" : "redirect:/admin/users/edit/" + user.getId();
